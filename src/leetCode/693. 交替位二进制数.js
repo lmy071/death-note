0510064e7911c4f5fbf871e0e1eeafe97c617068
Lmy <lmy071@icloud.com>
2026-03-12 10:39:11 +0800

    693

---
+++ src/leetCode/693. 交替位二进制数.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var hasAlternatingBits = function(n) {
+    let prev = 2;
+    while (n !== 0) {
+        const cur = n % 2;
+        if (cur === prev) {
+            return false;
+        }
+        prev = cur;
+        n = Math.floor(n / 2);
+    }
+    return true;
+};
